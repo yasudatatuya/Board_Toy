e6e8b7b93eb80f5074c6c79aa72ae87c325bfb3d
EC2 Default User <ec2-user@ip-172-31-23-22.ap-northeast-1.compute.internal>
2020-12-24 10:48:31 +0000

    migrate.reset

--- db/migrate/20201220070825_add_title_to_game_comments.rb
+++ db/migrate/20201220070825_add_title_to_game_comments.rb
@@ -1,5 +1,14 @@
 class AddTitleToGameComments < ActiveRecord::Migration[5.2]
   def change
-    add_column :game_comments, :title, :string
+    create_table "game_comments", force: :cascade do |t|
+      t.text "comment"
+      t.integer "user_id"
+      t.integer "game_id"
+      t.float "star"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "title"
+      t.float "rate"
+    end
   end
 end
